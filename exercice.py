8fb4735b10f769c4a63b30e2729862a6826e0122
Lydiare <73038866+Lydiare@users.noreply.github.com>
2022-10-17 10:25:14 -0400

    3.2 à moitié

--- exercice.py
+++ exercice.py
@@ -3,44 +3,57 @@
 
 def dissipated_power(voltage, resistance):
 	# TODO: Calculer la puissance dissipée par la résistance.
-	return 0
+	puissance = voltage**2/resistance
+	return puissance
 
 def orthogonal(v1, v2):
 	# TODO: Retourner vrai si les vecteurs sont orthogonaux, faux sinon.
-	v1[0] # Pour accéder au X
-	v1[1] # Pour accéder au Y
+	produit_scalaire = v1[0]*v2[0] + v1[1]*v2[1]
+	if produit_scalaire == 0:
+		return (f"Les vecteurs {v1} et {v2} sont orthogonaux")
+	else:
+		return False
 	pass
 
 def average(values):
 	# TODO: Calculer la moyenne des valeurs positives (on ignore les valeurs strictement négatives).
+	valeurs = 0
+	nombre_elements = 0
 	for v in values:
-		pass # La variable v contient une valeur de la liste.
+		if v >= 0:
+			valeurs += v
+			nombre_elements += 1
 
-def bills(value):
-	# TODO: Calculez le nombre de billets de 20$, 10$ et 5$ et pièces de 1$ à remettre pour représenter la valeur.
-	while value != 0:
-		if value >= 20:
-			pass
-		elif value >= 10:
-			pass
-		elif value >= 5:
-			pass
-		elif value >= 1:
-			pass
+	moyenne= valeurs / nombre_elements
+	return (f"La moyenne des valeurs non négatives est : {moyenne}")
 
-	return (twenties, tens, fives, twos, ones);
+	pass # La variable v contient une valeur de la liste.
 
-def format_base(value, base, digit_letters):
-	# Formater un nombre dans une base donné en utilisant les lettres fournies pour les chiffres<
-	# `digits_letters[0]` Nous donne la lettre pour le chiffre 0, ainsi de suite.
-	result = ""
-	abs_value = abs(value)
-	while abs_value != 0:
-		pass
-	if value < 0:
-		# TODO: Ne pas oublier d'ajouter '-' devant pour les nombres négatifs.
-		pass
-	return result
+# def bills(value):
+# 	# TODO: Calculez le nombre de billets de 20$, 10$ et 5$ et pièces de 1$ à remettre pour représenter la valeur.
+# 	while value != 0:
+# 		if value >= 20:
+# 			pass
+# 		elif value >= 10:
+# 			pass
+# 		elif value >= 5:
+# 			pass
+# 		elif value >= 1:
+# 			pass
+#
+# 	return (twenties, tens, fives, twos, ones);
+#
+# def format_base(value, base, digit_letters):
+# 	# Formater un nombre dans une base donné en utilisant les lettres fournies pour les chiffres<
+# 	# `digits_letters[0]` Nous donne la lettre pour le chiffre 0, ainsi de suite.
+# 	result = ""
+# 	abs_value = abs(value)
+# 	while abs_value != 0:
+# 		pass
+# 	if value < 0:
+# 		# TODO: Ne pas oublier d'ajouter '-' devant pour les nombres négatifs.
+# 		pass
+# 	return result
 
 
 if __name__ == "__main__":
